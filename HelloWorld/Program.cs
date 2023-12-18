1b1b65fd9b87cd2cab38494a3f7b3384388d2c85
Hleb Pashkevich <hleb.pashkevich@gmail.com>
2023-12-18 21:59:14 +0300

    HelloWorld App adding

---
+++ HelloWorld/Program.cs
@@ -0,0 +1,11 @@
+﻿// Потребовать ввод от пользователя
+Console.Write("Enter your name: ");
+
+// Считать введенное имя с консоли
+string userName = Console.ReadLine();
+
+// Вывести приветствие на консоль
+Console.WriteLine($"Hello {userName}!");
+
+// Ждать, пока пользователь нажмет клавишу перед выходом
+Console.ReadLine();
